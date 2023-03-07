293b0ec9de3dfe25143e3699d99629c08dd38b6f
sanjana8100 <sanjanams812@gmail.com>
2023-03-07 20:54:44 +0530

    added: UC4

--- LineComparison.java
+++ LineComparison.java
@@ -3,7 +3,7 @@ package com.bridgelabz;
 import java.util.Scanner;
 
 public class LineComparison {
-    public double LineInput(int LineNo){
+    public String LineInput(int LineNo){
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the values of co-ordinates x1 and y1 for line "+LineNo+": ");
         int x1= in.nextInt();
@@ -12,22 +12,28 @@ public class LineComparison {
         int x2= in.nextInt();
         int y2= in.nextInt();
         double length= Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        String len=Double.toString(length);
         System.out.println("The length of Line "+LineNo+" is: "+length);
         System.out.println("_____________________________________________________________");
-        return length;
+        return len;
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program on Main Branch.");
 
         LineComparison line= new LineComparison();
-        double length1= line.LineInput(1);
-        double length2= line.LineInput(2);
+        String length1= line.LineInput(1);
+        String length2= line.LineInput(2);
         System.out.println("********************************************************************");
-        if(length1 == length2)
-            System.out.println("Lengths of Line 1 and Line 2 are EQUAL.");
-        else if(length1 > length2)
-            System.out.println("Length of Line 1 is GREATER THAN Length of Line 2.");
-        else
-            System.out.println("Length of Line 1 is LESSER THAN Length of Line 2.");
+        int value= (length1.compareTo(length2));
+        switch (value){
+            case 0:
+                System.out.println("Lengths of Line 1 and Line 2 are EQUAL.");
+                break;
+            case 1:
+                System.out.println("Length of Line 1 is GREATER THAN Length of Line 2.");
+                break;
+            default:
+                System.out.println("Length of Line 1 is LESSER THAN Length of Line 2.");
+        }
     }
 }
