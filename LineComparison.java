d5b10de6c3c81a7a1bc13332aaeadb73ddb9a92d
sanjana8100 <sanjanams812@gmail.com>
2023-03-07 20:12:47 +0530

    added: UC1

--- LineComparison.java
+++ LineComparison.java
@@ -1,7 +1,18 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LineComparison {
     public static void main(String[] args) {
         System.out.println("Welcome to Line Comparison Computation Program on Main Branch.");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the values of co-ordinates x1 and y1 for a line:");
+        int x1= in.nextInt();
+        int y1= in.nextInt();
+        System.out.println("Enter the values of co-ordinates x2 and y2 for a line:");
+        int x2= in.nextInt();
+        int y2= in.nextInt();
+        double length= Math.sqrt((x2-x1)^2 + (y2-y1)^2);
+        System.out.println("The length of the given line is: "+length);
     }
 }
